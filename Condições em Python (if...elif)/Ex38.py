e737628de6865495d1a19575c50f5072d6e0de59
Bruno <brunosssss@gmail.com>
2021-11-17 12:44:38 -0300

    Qual o numero é maior

---
+++ Condições em Python (if...elif)/Ex38.py
@@ -0,0 +1,9 @@
+numA = int(input("Digite o numero 1: "))
+numB = int(input("Digite o numero 2: "))
+
+if numA > numB:
+    print (f"O PRIMEIRO numero é maior")
+elif numB > numA:
+    print (f"O SEGUNDO numero é maior")
+else:
+    print("Os numeros são IGUAIS")
